74b2aa35c52400e4ad174b67988b48c497d06a72
graceyue806 <zhangshiyuee@hotmail.com>
2015-06-22 01:51:42 -0400

    Adding Clumsy Bird Game Localization

---
+++ clumsy-bird/build/String_ZH.js
@@ -0,0 +1,19 @@
+var i18n = {	
+	'Your browser does not support HTML5 canvas.': '您的浏览器不支持HTML5画布。',
+	'data/img/logo.png': 'data/img_ZH/clumsybird.zh.png',
+	'data/img/gameover.png': 'data/img_ZH/gameover.zh.png',
+	'data/img/getready.png': 'data/img_ZH/getready.zh.png',
+	'data/img/new.png': 'data/img/new.png',
+	'data/img/share.png': 'data/img_ZH/share.zh.png',
+	'data/img/tweet.png': 'data/img_ZH/tweet.zh.png',
+	'Just made': '刚获得大笨鸟',
+	'steps on Clumsy Bird! Can you beat me? Try online here!': '分！敢跟我比拼吗？上网来试试吧！',
+	'My Clumsy Bird Score!': '我的大笨鸟分数！',
+	'Share to your friends': '推荐给你的朋友',
+	'Just made': '刚获得大笨鸟',
+	'steps on Clumsy Bird! Can you beat me? Try online here!': '分！敢跟我比拼吗？上网来试试吧！',
+	'PRESS SPACE OR CLICK LEFT MOUSE BUTTON TO START': '点击空格键或单击鼠标右键开始',
+	'PRESS "M" TO MUTE SOUND': '点击M键静音',
+	'Steps:': '总分：',
+	'Higher Step:': '最高分：'
+};
