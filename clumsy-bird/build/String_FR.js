504e1b35a8de35abfd8fc0ed714e215cf89dfcbb
graceyue806 <zhangshiyuee@hotmail.com>
2015-06-22 01:31:27 -0400

    Clumsy Bird

---
+++ clumsy-bird/build/String_FR.js
@@ -0,0 +1,19 @@
+var i18n = {
+	'Your browser does not support HTML5 canvas.': 'Votre navigateur ne supporte pas HTML5 canvas.',
+	'data/img/logo.png': 'data/img/logo.png',
+	'data/img/gameover.png': 'data/img_FR/gameover.fr.png',
+	'data/img/getready.png': 'data/img_FR/getready.fr.png',
+	'data/img/new.png': 'data/img_FR/new.fr.png',
+	'data/img/share.png': 'data/img_FR/share.fr.png',
+	'data/img/tweet.png': 'data/img_FR/tweet.fr.png',
+	'Just made': 'Je viens de passer',
+	'steps on Clumsy Bird! Can you beat me? Try online here!': 'étapes sur Clumsy Bird ! Pouvez-vous faire mieux ? Jouez en ligne ici !',
+	'My Clumsy Bird Score!': 'Mon score Clumsy Bird !',
+	'Share to your friends': 'Partager avec vos amis',
+	'Just made': 'Je viens de passer',
+	'steps on Clumsy Bird! Can you beat me? Try online here!': 'étapes sur Clumsy Bird ! Pouvez-vous faire mieux ? Jouez en ligne ici !',
+	'PRESS SPACE OR CLICK LEFT MOUSE BUTTON TO START': 'POUR COMMENÇER, APPUYEZ SUR ESPACE OU CLIQUEZ AVEC LE BOUTON GAUCHE DE LA SOURIS',
+	'PRESS "M" TO MUTE SOUND': 'APPUYEZ SUR "M" POUR COUPER LE SON',
+	'Steps:': 'Étapes passées',
+	'Higher Step:': 'Meilleur score:'
+};
